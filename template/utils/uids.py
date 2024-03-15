74636be49d579b522159c13b69de4054ae2e09b7
cjtf0991 <cjtf0991@proton.me>
2024-03-15 18:09:58 +0100

    remove vpermit limit entirely
    this will cause some verification errors but it avoids miners
    from being excluded if they make too much

--- template/utils/uids.py
+++ template/utils/uids.py
@@ -4,24 +4,17 @@ import bittensor as bt
 from typing import List
 
 
-def check_uid_availability(
-    metagraph: "bt.metagraph.Metagraph", uid: int, vpermit_tao_limit: int
-) -> bool:
-    """Check if uid is available. The UID should be available if it is serving and has less than vpermit_tao_limit stake
+def check_uid_availability(metagraph: "bt.metagraph.Metagraph", uid: int) -> bool:
+    """Check if uid is available. The UID should be available if it is serving.
     Args:
         metagraph (:obj: bt.metagraph.Metagraph): Metagraph object
         uid (int): uid to be checked
-        vpermit_tao_limit (int): Validator permit tao limit
     Returns:
         bool: True if uid is available, False otherwise
     """
     # Filter non serving axons.
     if not metagraph.axons[uid].is_serving:
         return False
-    # Filter validator permit > 1024 stake.
-    if metagraph.validator_permit[uid]:
-        if metagraph.S[uid] > vpermit_tao_limit:
-            return False
     # Available otherwise.
     return True
 
@@ -42,9 +35,7 @@ def get_random_uids(
     avail_uids = []
 
     for uid in range(self.metagraph.n.item()):
-        uid_is_available = check_uid_availability(
-            self.metagraph, uid, self.config.neuron.vpermit_tao_limit
-        )
+        uid_is_available = check_uid_availability(self.metagraph, uid)
         uid_is_not_excluded = exclude is None or uid not in exclude
 
         if uid_is_available:
