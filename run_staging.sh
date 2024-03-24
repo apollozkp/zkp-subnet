4ec3bafe56131efd8757709833e93c4cc19d9f86
cjtf0991 <cjtf0991@proton.me>
2024-03-24 13:07:28 +0100

    add libgmp-dev to staging script

--- run_staging.sh
+++ run_staging.sh
@@ -8,7 +8,7 @@ install_dependencies() {
         echo "Updating system packages..."
         sudo apt update
         echo "Installing required packages..."
-        sudo apt install --assume-yes make build-essential git clang curl libssl-dev llvm libudev-dev protobuf-compiler tmux
+        sudo apt install --assume-yes make build-essential git clang curl libssl-dev llvm libudev-dev protobuf-compiler tmux libgmp-dev
     }
 
     # Detect OS and call the appropriate function
